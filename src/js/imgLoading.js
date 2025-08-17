9cb8095fccbee2902dc3ee93038e3b5b67ba31e1
Vladyslav Popov <vlad_np@ukr.net>
2025-08-17 16:13:45 +0200

    add retina for hero

--- src/js/imgLoading.js
+++ src/js/imgLoading.js
@@ -1,6 +1,9 @@
 import heroMobile from '../img/hero/hero_2.jpg';
 import heroTablet from '../img/hero/hero_tablet_2.jpg';
 import heroDesktop from '../img/hero/hero_desktop_2.jpg';
+import heroMobile2x from '../img/hero/hero_2@2x.jpg';
+import heroTablet2x from '../img/hero/hero_tablet_2@2x.jpg';
+import heroDesktop2x from '../img/hero/hero_desktop_2@2x.jpg';
 
 document.addEventListener('DOMContentLoaded', () => {
   const hero = document.querySelector('.hero');
@@ -8,19 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const img = new Image();
   let bgUrl;
+  let bgUrl2x;
 
   if (window.innerWidth >= 1280) {
     bgUrl = heroDesktop;
+    bgUrl2x = heroDesktop2x;
   } else if (window.innerWidth >= 768) {
     bgUrl = heroTablet;
+    bgUrl2x = heroTablet2x;
   } else {
     bgUrl = heroMobile;
+    bgUrl2x = heroMobile2x;
   }
+
   img.onload = () => {
     hero.style.setProperty(
       '--main-bg',
       `linear-gradient(-144deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.25) 60%), url(${bgUrl})`
     );
+
+    hero.style.setProperty(
+      '--main-bg-2x',
+      `linear-gradient(-144deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.25) 60%), url(${bgUrl2x})`
+    );
+
     hero.classList.add('loaded');
   };
 
